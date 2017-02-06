e098903a61ecf587bdedb6f4fcd7f2b465b907d3
henne <henne@drunkenrecords.de>
2017-02-06 19:05:23 +0100

    removed image functions and therefore deasync and getpixel dependency

--- src/printer.js
+++ src/printer.js
@@ -2,8 +2,6 @@
 var util = require('util'),
 	EventEmitter = require('events').EventEmitter,
 	fs = require('fs'),
-	getPixels = require('get-pixels'),
-	deasync = require('deasync'),
 	async = require('async'),
 	helpers = require('./helpers'),
 	specialChars = require('./specialChars.js'),
@@ -294,71 +292,6 @@ Printer.prototype.printText = Printer.prototype.addText = function (text) {
 
 Printer.prototype.printLine = function (text) {
 	return this.printText(text).writeCommand(10);
-};
-
-Printer.prototype.printImage = function(path){
-	var done = false;
-
-	var _self = this;
-	getPixels(path, function(err, pixels){
-		if(!err){
-			var width = pixels.shape[0];
-			var height = pixels.shape[1];
-
-			if (width != 384 || height > 65635) {
-				throw new Error('Image width must be 384px, height cannot exceed 65635px.');
-			}
-
-			// contruct an array of Uint8Array,
-			// each Uint8Array contains 384/8 pixel samples, corresponding to a whole line
-			var imgData = [];
-			for (var y = 0; y < height; y++) {
-				imgData[y] = new Uint8Array(width/8);
-				for (var x = 0; x < (width/8); x++) {
-					imgData[y][x] = 0;
-					for (var n = 0; n < 8; n++) {
-						var r = pixels.get(x*8+n, y, 0);
-						var g = pixels.get(x*8+n, y, 1);
-						var b = pixels.get(x*8+n, y, 2);
-
-						var brightness = helpers.rgbToHsl(r, g, b)[2];
-						// only print dark stuff
-						if (brightness < 0.6) {
-							imgData[y][x] += (1 << n);
-						}
-					}
-				}
-			}
-
-			// send the commands and buffers to the printer
-			_self.printImageData(width, height, imgData);
-			// tell deasync getPixels is done
-			done = true;
-		}
-		else {
-			throw new Error(err);
-		}
-	});
-	// deasync getPixels
-	while(!done) {
-		deasync.runLoopOnce();
-	}
-	return this;
-};
-
-Printer.prototype.printImageData =function(width, height, imgData){
-	if (width != 384 || height > 65635) {
-		throw new Error('Image width must be 384px, height cannot exceed 65635px.');
-	}
-
-	// send the commands and buffers to the printer
-	var commands = [18, 118, height & 255, height >> 8];
-	for (var y = 0; y < imgData.length; y++) {
-		var buf = helpers.uint8ArrayToBuffer(imgData[y]);
-		commands.push(buf);
-	}
-	this.writeCommands(commands);
-	return this;
 };
 
 // Barcodes
